ead4c913728c8fedc304bb4bf79d3cb2a79a3a28
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2020-10-11 01:04:27 +0545

    signup button animation not working after it navigates from logout action has been fixed

--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { IfStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LoginUser, RegisterUser } from 'src/app/models/User';
 import { UserService } from 'src/app/services/user.service';
@@ -10,21 +10,30 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  constructor(private userService: UserService, private toastr: ToastrService, private router:Router) { }
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService,
+    private router: Router,
+    route: ActivatedRoute
+  ) {
+    route.params.subscribe(val => {
+      //Animation for signin and sign up toggle
+      const sign_in_btn = document.querySelector("#sign-in-btn");
+      const sign_up_btn = document.querySelector("#sign-up-btn");
+      const container = document.querySelector(".container");
+      sign_up_btn.addEventListener("click", () => {
+        container.classList.add("sign-up-mode");
+      });
+      sign_in_btn.addEventListener("click", () => {
+        container.classList.remove("sign-up-mode");
+      });
+    });
+  }
   ngOnInit(): void {
-    if(localStorage.getItem('token') !=null){
+    if (localStorage.getItem('token') != null) {
       this.router.navigate(['/home'])
     }
-    //Animation for signin and sign up toggle
-    const sign_in_btn = document.querySelector("#sign-in-btn");
-    const sign_up_btn = document.querySelector("#sign-up-btn");
-    const container = document.querySelector(".container");
-    sign_up_btn.addEventListener("click", () => {
-      container.classList.add("sign-up-mode");
-    });
-    sign_in_btn.addEventListener("click", () => {
-      container.classList.remove("sign-up-mode");
-    });
+
   }
   // Sign Up Part //
   signUpUserName: string = "";
@@ -76,26 +85,26 @@ export class RegistrationComponent implements OnInit {
     this.validPassword = false;
   }
   //SignIn Part
-  signInEmail:string="";
+  signInEmail: string = "";
   signInPassword: string = "";
-  loginModel:LoginUser
-  signIn(){
-    this.loginModel={
-      Email:this.signInEmail,
+  loginModel: LoginUser
+  signIn() {
+    this.loginModel = {
+      Email: this.signInEmail,
       Password: this.signInPassword
     }
-    this.userService.login(this.loginModel).subscribe((res:any)=> {
+    this.userService.login(this.loginModel).subscribe((res: any) => {
       localStorage.setItem("token", res.token);
       this.toastr.success("Login Sucessful", 'Thank You!');
       this.router.navigateByUrl("/home");
     },
-    (err) => {
-        if(err.status == 400){
+      (err) => {
+        if (err.status == 400) {
           this.toastr.warning("Incorrect username or password", 'Failed!');
         }
-        else{
+        else {
           this.toastr.error("Login Failed", 'Sorry!');
         }
-    })
+      })
   }
 }
